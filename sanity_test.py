c9f2c94f7b00cc31aa07895a26278e35eb814fdf
Colombe Siegenthaler <colombe.siegenthaler@env.ethz.ch>
2019-07-30 13:02:28 +0200

    Use begins package

--- sanity_test.py
+++ sanity_test.py
@@ -2,8 +2,23 @@
 # C.Siegenthaler, 2019
 
 
-from shell_interactions.py import sys_cmd
+from shell_interactions import sys_cmd
+import begin
+import os
+import pandas as pd
+
+wk_dir = '/project/s903/colombsi/plattform_comparison/Sandbox/'
 
 def cdo_process(filename) :
     return()
 
+
+@begin.start
+
+def run(path='/project/s903/nedavid/plattform_comparison/', expname = 'aerosol_REF_10y', \
+        file_sum = '/project/s903/colombsi/plattform_comparison/Platform_comparison.xlsx'):
+
+    os.chdir(wk_dir)
+
+
+    
