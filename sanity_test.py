a4d1d90772189470fa45b9cea59ca1c756e0a07d
Colombe Siegenthaler <colombe.siegenthaler@env.ethz.ch>
2019-07-30 17:00:41 +0200

    Read different files into panda dataframes compute standard deviation from timeserie of global mean file

--- sanity_test.py
+++ sanity_test.py
@@ -10,6 +10,8 @@ import pandas as pd
 def cdo_process(filename) :
     return()
 
+
+
 class table_ref:
     def __init__(self, file_summary):
         # tabs in the excel file
@@ -24,9 +26,6 @@ class table_ref:
         # keep track of the tab names
         self.tabs = tabs_table
 
-    def __setattr__(self, name, value):
-         raise Exception("This property is read only!")
-
 @begin.start
 
 #def run(exppath='/project/s903/nedavid/plattform_comparison/', expname = 'aerosol_REF_10y', \
@@ -34,12 +33,29 @@ class table_ref:
 #        wrk_dir = '/project/s903/colombsi/plattform_comparison/Sandbox/') :
 def run(exppath='/Users/colombsi/Documents/ECHAM-runs/', expname='aerosol_REF_10y',
         file_summary = '/Users/colombsi/Documents/sanity-test-hammoz/Platform_comparison.xlsx',
-        wrk_dir = '/Users/colombsi/Documents/Sandbox/'):
+        wrk_dir = '/Users/colombsi/Documents/Sandbox/sanity_test'):
 
+    # prepare working directory
+    os.makedirs(wrk_dir,exist_ok=True)
     os.chdir(wrk_dir)
-    data = table_ref(file_summary)
 
-    print(data.exp)
+    # data of reference experiments
+    data_ref_file = table_ref(file_summary)
+
+    # global mean new experiment
+    p_globmean_newexp = os.path.join(exppath, expname , 'Data/analysis_{}_2003-2012.txt'.format(expname))
+    data_newexp_glob = pd.read_csv(p_globmean_newexp, sep='\s+', header=1)
+
+    # standard deviation new experiment
+    tmp_timeser_newexp = p_globmean_newexp.replace('analysis', 'timeser').rstrip('.txt')
+    p_timeser_newexp = os.path.join(wrk_dir, (os.path.basename(tmp_timeser_newexp) + '.txt'))
+    # compute std deviation
+    sys_cmd('cdo infov -timstd {}.nc > {}'.format(tmp_timeser_newexp, p_timeser_newexp))
+    data_newexp_std = pd.read_csv(p_timeser_newexp, sep=' : ', usecols=[2,3], skiprows = [0], names = ['std','name'])
+
+    data_newexp_std = data_newexp_std.reindex(columns=['name','std'])
+    print(data_newexp_std)
+#    print(data_newexp_glob)
 
 
 
