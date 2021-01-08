d2b66c4bf94b85d059623e433e4b40a01930ebfc
Jonas Jucker <joni.jucker@yahoo.de>
2021-01-08 17:43:59 +0100

    draft test

---
+++ test.py
@@ -0,0 +1,15 @@
+import  pandas as pd
+import xarray as xr
+import numpy as np
+
+time = pd.date_range("2000-01-01", freq="D", periods=366)
+lat = np.arange(10,20,0.1)
+lon = np.arange(10,20,0.1)
+
+test = xr.DataArray(np.random.rand(366,100,100), coords=[time,lat, lon], dims=['time','lat','lon'],name='test1')
+print(test)
+months = range(12)
+for m in months:
+    test.sel(time=test['time.month'] == m)
+    test.to_netcdf(f'{m}.nc')
+
