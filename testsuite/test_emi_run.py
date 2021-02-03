e97ac4397786264f099650dfe385a19612eb6008
juckerj <jonas.jucker@env.ethz.ch>
2021-02-03 09:49:05 +0100

    Pep8 complete

--- testsuite/test_emi_run.py
+++ testsuite/test_emi_run.py
@@ -1,21 +1,28 @@
 import testsuite.utils as utils
 
+
 def test_emi_embed():
 
     input_dir = 'testsuite/data'
     exp_name = 'run_embed'
 
     files_generated = utils.generate_data(input_dir,exp_name,'emi_A','SO2')
-    files_generated.extend(utils.generate_data(input_dir,exp_name,'emi_B','CO2'))
+    files_generated.extend(utils.generate_data(input_dir,
+                                               exp_name,
+                                               'emi_B',
+                                               'CO2'))
 
     cmd = 'python paths_init.py -pr {} -ts'.format(input_dir)
-    status, _ =utils.shell_cmd(cmd)
+    status, _ = utils.shell_cmd(cmd)
 
     assert status == 0, 'paths_init.py failed'
-    
-    cmd = 'python sanity_test.py -v -e {} -t emi --p_ref_csv_files testsuite/ref --f_vars_to_extract vars_emi_test.csv --spinup 0 -c -ts'.format(exp_name)
 
-    status, _ =utils.shell_cmd(cmd)
+    cmd = 'python sanity_test.py -v -e {} -t emi \
+            --p_ref_csv_files testsuite/ref \
+            --f_vars_to_extract vars_emi_test.csv \
+            --spinup 0 -c -ts'.format(exp_name)
+
+    status, _ = utils.shell_cmd(cmd)
 
     utils.delete_data(files_generated)
 
@@ -28,22 +35,29 @@ def test_emi_chained():
     exp_name = 'run_chained'
 
     files_generated = utils.generate_data(input_dir,exp_name,'emi_A','SO2')
-    files_generated.extend(utils.generate_data(input_dir,exp_name,'emi_B','CO2'))
+    files_generated.extend(utils.generate_data(input_dir,
+                                               exp_name,
+                                               'emi_B',
+                                               'CO2'))
 
     cmd = 'python paths_init.py -pr {} -ts'.format(input_dir)
-    status, _ =utils.shell_cmd(cmd)
+    status, _ = utils.shell_cmd(cmd)
 
     assert status == 0, 'paths_init.py failed'
-    
-    cmd = 'python process_data.py -v -e {} -t emi --f_vars_to_extract vars_emi_test.csv --spinup 0 -c'.format(exp_name)
 
-    status, _ =utils.shell_cmd(cmd)
+    cmd = 'python process_data.py -v -e {} -t emi \
+            --f_vars_to_extract vars_emi_test.csv \
+            --spinup 0 -c'.format(exp_name)
+
+    status, _ = utils.shell_cmd(cmd)
 
     assert status == 0, 'process_data.py failed'
 
-    cmd = 'python perform_test.py -v -e {} -t emi --p_ref_csv_files testsuite/ref --f_vars_to_extract vars_emi_test.csv -ts'.format(exp_name)
+    cmd = 'python perform_test.py -v -e {} -t emi \
+            --p_ref_csv_files testsuite/ref \
+            --f_vars_to_extract vars_emi_test.csv -ts'.format(exp_name)
 
-    status, _ =utils.shell_cmd(cmd)
+    status, _ = utils.shell_cmd(cmd)
 
     utils.delete_data(files_generated)
 
--- testsuite/test_pep8.py
+++ testsuite/test_pep8.py
@@ -1,33 +1,47 @@
 import testsuite.utils as utils
 
-# The following picky coding rules are ignored:
+'''
+The following picky coding rules are ignored:
 
 ############# ERROR CODES ####################
 
-# E265: block comment should start with '#'
-# E201: whitespace after '('
-# E221: multiple spaces before operator
-# E231: missing whitespace after ',' ';' or ':'
-# E241: multiples spaces after ','
-# E722: do not use bare except, specify exception instead
-# E731: do not assign a lambda expression, use a def 
+  E265: block comment should start with '#'
+  E201: whitespace after '('
+  E221: multiple spaces before operator
+  E231: missing whitespace after ',' ';' or ':'
+  E241: multiples spaces after ','
+  E722: do not use bare except, specify exception instead
+  E731: do not assign a lambda expression, use a def 
+  E225 missing whitespace around operator
 
 ############# WARNINGS #######################
 
-# W291: trailing whitespace
-# W504: line break after binary operator
+  W291: trailing whitespace
+  W504: line break after binary operator
+
+'''
+
 
 def test_main():
-    cmd = 'python testsuite/pycodestyle.py --exclude=prepare_csvfiles_ref_echam.py --ignore=E265,W291,E201,E221,E231,E241,E722,W504 *.py'
+    cmd = 'python testsuite/pycodestyle.py \
+            --exclude=prepare_csvfiles_ref_echam.py \
+            --ignore=E265,W291,E201,E221,E231,E241,E722,W504 *.py'
     status, _ =utils.shell_cmd(cmd)
     assert status == 0, 'Violations of Pep-8-standard found!'
+
 
 def test_lib():
-    cmd = 'python testsuite/pycodestyle.py --exclude=paths.py --ignore=E265,W291,E201,E221,E231,E241,E722,W504,E731 lib/*.py'
+    cmd = 'python testsuite/pycodestyle.py --exclude=paths.py \
+            --ignore=E265,W291,E201,E221,E231,E241,E722,W504,E731 \
+            lib/*.py'
     status, _ =utils.shell_cmd(cmd)
     assert status == 0, 'Violations of Pep-8-standard found!'
 
-#def test_testsuite():
-#    cmd = 'python testsuite/pycodestyle.py --ignore=E265,W291,E201,E221,E231,E241,E722,W504 testsuite/*.py'
-#    status, _ =utils.shell_cmd(cmd)
-#    assert status == 0, 'Violations of Pep-8-standard found!'
+
+def test_testsuite():
+    cmd = 'python testsuite/pycodestyle.py \
+            --exclude=test_fldcor_run.py \
+            --ignore=E225,E265,W291,E201,E221,E231,E241,E722,W504 \
+            testsuite/*.py'
+    status, _ =utils.shell_cmd(cmd)
+    assert status == 0, 'Violations of Pep-8-standard found!'
